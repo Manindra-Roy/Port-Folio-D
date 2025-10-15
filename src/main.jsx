e0ba0ac7b8e4a3439c57bfc6f64e5d0a62b84eb6
Manindra Roy <anonymous.secret.in@gmail.com>
2025-10-15 01:46:40 +0000

    Speed Insights v-2

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
