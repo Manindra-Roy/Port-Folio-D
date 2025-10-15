2bb9d0186d933d9c4c2514b90453b31efa9a9181
Manindra Roy <anonymous.secret.in@gmail.com>
2025-10-15 01:37:37 +0000

    Speed Insights v-1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Works from "./sections/Works";
 import ContactSummary from "./sections/ContactSummary";
 import Contact from "./sections/Contact";
 import { useProgress } from "@react-three/drei";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   const { progress } = useProgress();
