bcb2aa16497378f89c37bdd4ebef5bc16fcb63b0
Manindra Roy <anonymous.secret.in@gmail.com>
2025-10-20 16:20:25 +0000

    sitemap add

---
+++ sitemap-generator.js
@@ -0,0 +1,33 @@
+// sitemap-generator.js
+
+import fs from 'fs'; // This is the only line that changed
+
+const websiteUrl = 'https://www.techtentacles.xyz';
+
+// Add all the routes/pages you want in your sitemap here
+const pages = [
+  '/',
+  '/about',
+  '/contact'
+];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages
+    .map(page => {
+      const path = page === '/' ? '' : page;
+      return `
+    <url>
+        <loc>${`${websiteUrl}${path}`}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>${page === '/' ? '1.0' : '0.8'}</priority>
+    </url>
+      `;
+    })
+    .join('')}
+</urlset>`;
+
+fs.writeFileSync('public/sitemap.xml', sitemap);
+
+console.log('✅ sitemap.xml generated successfully!');
